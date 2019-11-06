42362ada099e2b7af04eb04beb7ed4a429b8a957
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-06 07:30:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(x)
+x = 10
+while x > 0  
+  puts "#{x} SECOND(S)!"
+  x -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end 
 
-def countdown
-end
+#def countdown
+#end
+def countdown_with_sleep(t)
+  loop do
+    t = Time.now 
+   # sleep(t + 1 - time.now)
+  puts "#{t} SECOND(S)!"
+  end 
+end 
